a52c6d2a23f32ae6f8086084857665cdf39eafd5
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-10 01:02:17 +0300

    Create help.js

---
+++ commands/help.js
@@ -0,0 +1,15 @@
+const botconfig = require("./botconfig.json");
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+  let sEmbed = new Discord.RichEmbed()
+  .setColor("#5780cd")
+  .setTitle("Bot Help")
+  .addField("This is just a test!!!", true)
+  message.channel.send({embed: sEmbed});
+}
+
+module.exports.config = {
+  name: "help",
+  aliases: ["h", "helpme"]
+}
