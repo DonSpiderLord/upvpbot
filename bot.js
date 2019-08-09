2b541b980e42f913173a85901ca6e41dc147511e
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-09 21:50:42 +0300

    IDEK

--- bot.js
+++ bot.js
@@ -1,12 +1,11 @@
 const Discord = require('discord.js');
-
 const client = new Discord.Client();
-
+client.login(process.env.BOT_TOKEN);
  
 
 client.on('ready', () => {
 
-    console.log('I am ready for work!');
+    console.log('Ready for work!');
 
 });
 
@@ -30,41 +29,6 @@ function processCommand(receivedMessage) {
     console.log("Arguments: " + arguments)
 
 //=======================================Changes are done under this line=======================================
-
-if (message.content.startsWith("$kick")) {
-
-if (!message.member.roles.find("Owner", "Co-Owner", "Manager", "Admin", "Developer", "Moderator", "Build Manager"))
-    return;
-// Easy way to get member object though mentions.
-var member = message.mentions.members.first();
-// Kick
-member.kick().then((member) => {
-    // Successmessage
-    message.channel.send(":wave: " + member.displayName + " has been successfully kicked :point_right: ");
-}).catch(() => {
-    // Failmessage
-    message.channel.send("Access Denied");
-});
+if (primaryCommand == 'ping'){
+  message.channel.send("Ping, Ping, Pong!");
 }
-};
-client.on("message", (message) => {
-if (message.content.startsWith("$ban")) {
-
-    if (!message.member.roles.find("Owner", "Co-Owner", "Manager", "Admin", "Developer", "Moderator", "Build Manager"))
-        return;
-
-    // Easy way to get member object though mentions.
-    var member = message.mentions.members.first();
-    // ban
-    member.ban().then((member) => {
-        // Successmessage
-        message.channel.send(":wave: " + member.displayName + " has been successfully banned https://gfycat.com/playfulfittingcaribou :point_right: ");
-    }).catch(() => {
-        // Failmessage
-        message.channel.send("Access Denied");
-    });
-}
-});
-
-// THIS  MUST  BE  THIS  WAY, THIS IS FOR AUTHENTICATION!
-client.login(process.env.BOT_TOKEN);
