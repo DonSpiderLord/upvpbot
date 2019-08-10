d487223214644a5129652ba2d48640fa1b2dfad7
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-10 12:39:19 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,15 +1,46 @@
 const botconfig = require("../botconfig.json");
 const Discord = require("discord.js");
+const prefix = botconfig.prefix
 
 module.exports.run = async (bot, message, args) => {
-  let sEmbed = new Discord.RichEmbed()
-  .setColor("#5780cd")
-  .setTitle("Bot Help")
-  .addField("This is just a test!!!", true)
-  message.channel.send({embed: sEmbed});
+    if (args[0] == "help") return message.channel.send(`Just do ${prefix}help instead.`)
+
+    if (args[0]) {
+      let command = args[0];
+      if(bot.commands.has(command)) {
+        command = bot.commands.get(command);
+        var SHembed = new Discord.RichEmbed()
+        .setColor()
+        .setAuthor(`U-PvP Discord Admin Help`, message.guild.iconURL)
+        .setDescription(`The bot prefix is: ${prefix}\n\n**Command:** ${command.config.name}\n**Description:** ${command.config.description || "No Description"}\n**Usage:** ${command.config.usage || "No Usage"}\n**Accessable by:** ${command.config.accessableby || "Members"}\n**Aliases:** ${command.config.noalias || command.config.aliases}`)
+      }
+    }
+    if (!args[0]) {
+      message.delete()
+      let embed = new Discord.RichEmbed()
+      .setAuthor(`U-PvP Discord Admin Help`, message.guild.iconURL)
+      .setColor()
+      .setDescription(`${message.author.username} check your dm's!`)
+
+      let Sembed = new Discord.RichEmbed()
+      .setColor()
+      .setAuthor(`U-PvP Discord Admin Help`, message.guild.iconURL)
+      .setThumbnail(bot.user.displayAvatarURL)
+      .setTimestamp()
+      .setDescription(`These are the available commands for the U-PvP Discord Admin!\nThe bot prefix is ${prefix}`)
+      .addField(`Commands:`, "``help``")
+      .setFooter("U-PvP Discord Admin Help", bot.user.displayAvatarURL)
+      message.channel.send(embed).then(m => m.delete(10000));
+      message.author.send(Sembed)
+
+    }
 }
 
 module.exports.config = {
   name: "help",
-  aliases: ["h", "helpme"]
+  aliases: ["h", "helpme"],
+  usage: "u.help",
+  description: "",
+  //noalias: "No Aliases",
+  accessableby: "Members"
 }
