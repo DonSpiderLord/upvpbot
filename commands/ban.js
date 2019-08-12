ac3a19397e60622a611af68eb30ca3abf80f880d
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-12 13:06:27 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -10,14 +10,14 @@ module.exports.run = async (bot, message, args) => {
         message.delete()
         banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:`).then(() => 
         member.ban({reason: 'They were bad!',}))
-        message.channel.send(`${mutee.user.username} was successfully banned.`)
+        message.channel.send(`${banee.user.username} was successfully banned.`)
 
         let Bembed = new Discord.RichEmbed()
         .setColor("#0890d4")
         .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .addField("Moderation:", "Ban")
-        .addField("Banned Person:", mutee.user.username)
+        .addField("Banned Person:", banee.user.username)
         .addField("Moderator:", message.author.username)
         .addField("Date:", message.createdAt.toLocaleString())
 
