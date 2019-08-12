e93d4ce72cade659c06bfa153d1ea7fb8b7e2e26
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-12 10:33:45 +0300

    Update unmute.js

--- commands/unmute.js
+++ commands/unmute.js
@@ -3,31 +3,27 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
     if(message.member.roles.some(r=>["Owner"].includes(r.name)) ) {
       let mutee = message.mentions.members.first() || message.guild.members.get(args[0]);
-      if(!mutee) return message.channel.send("Please supply a user to mute!");
-
-      let reason = args.slice(1).join(" ");
-      if(!reason) reason = "Please supply a reason to mute!"
+      if(!mutee) return message.channel.send("Please supply a user to unmute!");
 
       let muterole = message.guild.roles.find(r => r.name === "Muted")
       if(!muterole) return message.channel.send("There is no mute role!");
 
       mutee.removeRole(muterole.id).then(() => {
         message.delete()
-        mutee.send(`Hello, you have been unmuted in ${message.guild.name} for: ${reason}`)
+        mutee.send(`Hello, you have been unmuted in ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:`)
         message.channel.send(`${mutee.user.username} was successfully unmuted.`)
       })
 
-      let embed = new Discord.RichEmbed()
+      let UMembed = new Discord.RichEmbed()
       .setColor("")
       .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+      .setThumbnail(bot.user.displayAvatarURL)
       .addField("Moderation:", "Unmute")
       .addField("Muted Person:", mutee.user.username)
       .addField("Moderator:", message.author.username)
-      .addField("Reason:", reason)
       .addField("Date:", message.createdAt.toLocaleString())
 
-      let sChannel = message.guild.channels.find(c => c.name === "mod-logs")
-      sChannel.send(embed)
+      bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: UMembed});
 
   } else {
 
