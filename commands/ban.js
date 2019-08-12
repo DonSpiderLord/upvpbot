2866ae886a0725dee996facf49d9307832d6d3c9
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-12 13:03:55 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -8,23 +8,20 @@ module.exports.run = async (bot, message, args) => {
       const member = message.guild.member(banee)
       if (member){
         message.delete()
-        banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:`)
-        message.channel.send(`${banee.user.username} was successfully banned.`)
+        banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:`).then(() => 
+        member.ban({reason: 'They were bad!',}))
+        message.channel.send(`${mutee.user.username} was successfully banned.`)
 
         let Bembed = new Discord.RichEmbed()
         .setColor("#0890d4")
         .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .addField("Moderation:", "Ban")
-        .addField("Banned Person:", banee.user.username)
+        .addField("Banned Person:", mutee.user.username)
         .addField("Moderator:", message.author.username)
         .addField("Date:", message.createdAt.toLocaleString())
 
         bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: Bembed});
-
-        member.ban({
-          reason: 'They were bad!',
-        })
       }
     } else {
 
