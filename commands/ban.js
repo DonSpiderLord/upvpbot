d4e35b324e05c17757f329210bf6b1a33bf89f24
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-12 10:51:57 +0300

    Add files via upload

---
+++ commands/ban.js
@@ -0,0 +1,44 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if(message.member.roles.some(r=>["Owner"].includes(r.name)) ) {
+      let banee = message.mentions.members.first() || message.guild.members.get(args[0]);
+      if(!banee) return message.channel.send("Please supply a user to ban!");
+
+      const member = message.guild.member(banee)
+      if (member){
+        message.delete()
+        banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:`)
+        message.channel.send(`${mutee.user.username} was successfully banned.`)
+
+        let Bembed = new Discord.RichEmbed()
+        .setColor("#0890d4")
+        .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+        .setThumbnail(bot.user.displayAvatarURL)
+        .addField("Moderation:", "Ban")
+        .addField("Banned Person:", mutee.user.username)
+        .addField("Moderator:", message.author.username)
+        .addField("Date:", message.createdAt.toLocaleString())
+
+        bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: Bembed});
+
+        member.ban({
+          reason: 'They were bad!',
+        })
+      }
+    } else {
+
+        return message.channel.send("You don't have permission to use this command!");
+
+    }
+}
+
+module.exports.config = {
+  name: "ban",
+  aliases: ["banish"],
+  usage: "u.ban",
+  description: "Bans Specified User.",
+  //noalias: "No Aliases",
+  accessableby: "Owner"
+
+}
