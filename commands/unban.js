bb7278a548c4959ce8c3877e763cd97748e49181
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-14 01:21:00 +0300

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -34,7 +34,7 @@ module.exports.run = async (bot, message, args) => {
 module.exports.config = {
   name: "unban",
   aliases: ["forgive"],
-  usage: "u.unban <@user>",
+  usage: "u.unban <id>",
   description: "Unbans Specified User.",
   //noalias: "No Aliases",
   accessableby: "Owner"
