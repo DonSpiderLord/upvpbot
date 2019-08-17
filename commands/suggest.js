868037b219228b44e7a48fbb08da0663295f1cb9
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-17 10:14:59 +0300

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -13,16 +13,14 @@ module.exports.run = async (bot, message, args) => {
 
         let Rembed = new Discord.RichEmbed()
         .setColor("#13edc5")
-        .setAuthor(`${message.guild.name} Reports`, message.guild.iconURL)
+        .setAuthor(`${message.guild.name} Suggestions`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
-        .addField("Type:", "Report")
-        .addField("Reported Person:", rUser)
-        .addField("Reason:", reason)
-        .addField("Server:", server)
+        .addField("Type:", "Suggestion")
+        .addField("Reported Person:", suggestion)
         .addField("Reporter:", author)
         .addField("Date:", message.createdAt.toLocaleString())
 
-        bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: Rembed});
+        bot.guilds.get('607885235719372801').channels.get('608645820287418394').send({embed: Rembed});
         Rembed.react('👍')
         Rembed.react('👎')
     } else if(message.member.roles.some(r=>["SuggestionBan"].includes(r.name)) ){
