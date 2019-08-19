8cb58bc8ab210aa7eff1802bbda31931eac17fbe
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-19 09:38:29 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -37,11 +37,25 @@ bot.on("message", async message =>{
         if (message.content.includes(profanities[x])){
             await message.reply("Don't say that here! :angry:")     
           
+            let warnedpers = message.author.user.username;
             let ChatFilterEmbed = new Discord.RichEmbed()
             .setColor("#bf1711")
-            .setTitle(`${message.author} Has Been Warned For Language!`);
+            .setTitle(`${warnedpers} Has Been Warned For Language!`);
             message.channel.send({embed: ChatFilterEmbed});  
             message.delete();
+          
+            let ChatFilterModEmbed = new Discord.RichEmbed()
+            .setColor("#bf1711")
+            .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+            .setThumbnail(bot.user.displayAvatarURL)
+            .addField("Moderation:", "Warn")
+            .addField("Warner Person:", warnedpers)
+            .addField("Reason:", "Language")
+            .addField("Moderator:", bot.user.username)
+            .addField("Date:", message.createdAt.toLocaleString())
+
+            bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: Wembed});
+            
             return;     
   }
 }
