5b6e5fc6c1b3149cb60e794da7d1855097b89dfa
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-19 10:09:23 +0300

    Update purge.js

--- commands/purge.js
+++ commands/purge.js
@@ -1,15 +1,21 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-    const amount = args[0];
-    if (!amount) return message.reply('You must specify an amount to delete! (Min: 2 Max: 100)');
-
-    message.channel.fetchMessages({
-     limit: 100,
-   }).then((messages) => {
-      message.channel.bulkDelete(amount).catch(error => console.log(error.stack));
+    if(message.member.roles.some(r=>["Owner"].includes(r.name)) ) {
+        const amount = args[0];
+        if (!amount) return message.reply('You must specify an amount to delete! (Min: 2 Max: 100)');
+        message.channel.fetchMessages({
+        limit: 100,
+        }).then((messages) => {
+        message.channel.bulkDelete(amount).catch(error => console.log(error.stack));
  });
 }
+    }
+    else{
+        
+        return message.channel.send("You don't have permission to use this command!");
+   
+} 
 
 module.exports.config = {
   name: "purge",
