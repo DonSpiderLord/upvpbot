c165fccbaec70e0449c0d5cc04dcd455d70fcc78
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-19 16:21:32 +0300

    Add files via upload

---
+++ commands/join.js
@@ -0,0 +1,23 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+        if(message.member.voiceChannel){
+
+          if(!message.guild.voiceConnection){
+            message.member.voiceChannel.join()
+                .then(connection => {
+                    message.reply("Succesfully Joined Voice Channel");
+                })
+          }
+        }
+}
+
+module.exports.config = {
+  name: "join",
+  aliases: ["connect"],
+  usage: "u.join",
+  description: "Join voice channel!",
+  //noalias: "No Aliases",
+  accessableby: "Anyone"
+
+}
---
+++ commands/leave.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+        if(message.member.voiceChannel){
+          if(!message.guild.voiceConnection){
+            message.member.voiceChannel.leave()
+                .then(connection => {
+                    message.reply("Succesfully Left Voice Channel");
+                })
+          }
+        }
+}
+
+module.exports.config = {
+  name: "leave",
+  aliases: ["disconnect"],
+  usage: "u.leave",
+  description: "Leave voice channel!",
+  //noalias: "No Aliases",
+  accessableby: "Anyone"
+
+}
