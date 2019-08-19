b7611439bac9533530ebf153762dc0c8c91dc068
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-19 18:23:30 +0300

    Update join.js

--- commands/join.js
+++ commands/join.js
@@ -1,6 +1,25 @@
 const Discord = require("discord.js");
-const opus = require("opusscript");
-const ffmpeg = require("ffmpeg");
+var opusscript = require("opusscript");
+ 
+// 48kHz sampling rate, 20ms frame duration, stereo audio (2 channels)
+var samplingRate = 48000;
+var frameDuration = 20;
+var channels = 2;
+ 
+// Optimize encoding for audio. Available applications are VOIP, AUDIO, and RESTRICTED_LOWDELAY
+var encoder = new opusscript(samplingRate, channels, opusscript.Application.AUDIO);
+ 
+var frameSize = samplingRate * frameDuration / 1000;
+ 
+// Get PCM data from somewhere and encode it into opus
+var pcmData = new Buffer(pcmSource);
+var encodedPacket = encoder.encode(pcmData, frameSize);
+ 
+// Decode the opus packet back into PCM
+var decodedPacket = encoder.decode(encodedPacket);
+ 
+// Delete the encoder when finished with it (Emscripten does not automatically call C++ object destructors)
+encoder.delete();
 
 module.exports.run = async (bot, message, args) => {
         if(message.member.voiceChannel){
