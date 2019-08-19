be7259b8c55e887214e9f5d882c7969eecec6438
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-19 08:22:23 +0300

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -13,15 +13,19 @@ module.exports.run = async (bot, message, args) => {
         message.delete()
         message.channel.send(`Your suggestion has been recorded! Vote in #suggestions!`)
 
-        let Rembed = new Discord.RichEmbed()
+        let Sembed = new Discord.RichEmbed()
         .setColor("#13edc5")
         .setAuthor(`${message.guild.name} Suggestions`, message.guild.iconURL)
         .setThumbnail(bot.user.displayAvatarURL)
         .addField("Type:", "Suggestion")
         .addField("Suggestion:", suggestion)
         .addField("Reporter:", author)
-
-        bot.guilds.get('607885235719372801').channels.get('608645820287418394').send({embed: Rembed});
+        
+        bot.guilds.get('607885235719372801').channels.get('608645820287418394').send({embed: Sembed}).then(embedMessage => {
+        embedMessage.react("👍");
+        embedMessage.react("👎");
+        });
+        
     } else if(message.member.roles.some(r=>["SuggestionBan"].includes(r.name)) ){
 
       return message.channel.send("You have been banned from using this command!");
