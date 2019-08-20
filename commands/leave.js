fb644de68027f375f22468274f2659ab83537c81
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-20 13:54:52 +0300

    Update leave.js

--- commands/leave.js
+++ commands/leave.js
@@ -2,7 +2,7 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
           if(message.guild.voiceConnection){
-            message.member.voiceConnection.disconnect();
+            message.guild.voiceConnection.disconnect();
           } else {
                 message.reply("I must be in voice channel to leave it!");
           }
