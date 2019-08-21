d84b99ca7bd22a35bc8fc86160f6a84b6549bb72
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-21 09:52:17 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,25 +3,19 @@ const Discord = require("discord.js");
 const prefix = botconfig.prefix;
 const badwords = require("./badwords.json");
 var profanities = badwords.profanities;
-
 const bot = new Discord.Client({disableEveryone: true});
 bot.on("ready", async () =>{
   console.log(`${bot.user.username} is online!`);
 })
-
 const fs = require("fs");
 bot.commands = new Discord.Collection();
 bot.aliases = new Discord.Collection();
-
 fs.readdir("./commands", (err, files) => {
-
   if(err) console.log(err)
-
   let jsfile = files.filter(f => f.split(".").pop() === "js")
   if(jsfile.length <= 0) {
     return console.log("[LOGS] Couldn't Find Commands!");
   }
-
   jsfile.forEach((f, i) => {
     let pull = require(`./commands/${f}`);
     bot.commands.set(pull.config.name, pull);
@@ -30,7 +24,6 @@ fs.readdir("./commands", (err, files) => {
     });
   });
 });
-
 bot.on("message", async message =>{
   if (message.author.bot || message.channel.type === "dm") return;
   for (x = 0; x < profanities.length; x++) {
@@ -41,8 +34,8 @@ bot.on("message", async message =>{
             .setColor("#bf1711")
             .setTitle(`${message.author.username} Has Been Warned For Language!`);
             message.channel.send({embed: ChatFilterEmbed});  
-              //message.delete();
-          
+            //message.delete();
+
             let ChatFilterModEmbed = new Discord.RichEmbed()
             .setColor("#bf1711")
             .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
@@ -52,7 +45,6 @@ bot.on("message", async message =>{
             .addField("Reason:", "Language")
             .addField("Moderator:", bot.user.username)
             .addField("Date:", message.createdAt.toLocaleString())
-
             bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: ChatFilterModEmbed});
             
             return;     
@@ -60,10 +52,7 @@ bot.on("message", async message =>{
 }
   let args = message.content.slice(prefix.length).split(' ');
   let cmd = args.shift().toLowerCase();
-
-let commandfile = bot.commands.get(cmd.slice(0)) || bot.commands.get(bot.aliases.get(cmd.slice(0)))
-if (commandfile) commandfile.run(bot,message,args)
-
+  let commandfile = bot.commands.get(cmd.slice(0)) || bot.commands.get(bot.aliases.get(cmd.slice(0)))
+  if (commandfile) commandfile.run(bot,message,args)
 })
-
 bot.login(process.env.BOT_TOKEN);
