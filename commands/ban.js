dcbf2d900f463b1f8160233effb4423478743fac
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-23 14:54:11 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -1,8 +1,7 @@
 module.exports = {
 	name: 'ban',
-	description: 'Bans Specified User!',
-  	usage: "u.ban <@user>"
-	.execute(message, args) {
-		message.channel.send('Pong!');
+	description: 'Ping!',
+	execute(message) {
+		message.channel.send('Pong.');
 	},
 };
