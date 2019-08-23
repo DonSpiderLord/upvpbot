76f30479a219a486e8804e07b0c0aa036f7c8266
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-23 15:11:10 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -3,34 +3,34 @@ const Discord = require('discord.js');
 const botconfig = require("./botconfig.json");
 const prefix = botconfig.prefix;
 
-const bot = new Discord.Client();
-bot.commands = new Discord.Collection();
+const client = new Discord.Client();
+client.commands = new Discord.Collection();
 
 const commandFiles = fs.readdirSync('./commands').filter(file => file.endsWith('.js'));
 
 for (const file of commandFiles) {
 	const command = require(`./commands/${file}`);
-	bot.commands.set(command.name, command);
+	client.commands.set(command.name, command);
 }
 
-bot.once('ready', () => {
+client.once('ready', () => {
 	console.log('U-PvP Network Discord Admin Is READY!');
 });
 
-bot.on('message', message => {
-	if (!message.content.startsWith(prefix) || message.author.bot) return;
+client.on('message', message => {
+	if (!message.content.startsWith(prefix) || message.author.client) return;
 
 	const args = message.content.slice(prefix.length).split(/ +/);
 	const command = args.shift().toLowerCase();
 
-	if (!bot.commands.has(command)) return;
+	if (!client.commands.has(command)) return;
 
 	try {
-		bot.commands.get(command).execute(message, args);
+		client.commands.get(command).execute(message, args);
 	} catch (error) {
 		console.error(error);
 		message.reply('There was an error trying to execute that command!');
 	}
 });
 
-bot.login(process.env.BOT_TOKEN);
+client.login(process.env.BOT_TOKEN);
