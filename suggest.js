bb82f48b2e561129afd66eed3766b31d09e2f45b
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-26 11:53:20 +0300

    Create suggest.js

---
+++ suggest.js
@@ -0,0 +1,35 @@
+const Discord = require("discord.js");
+
+module.exports = {
+	name: 'suggest',
+	description: 'Suggestion Command',
+	execute(message, args) {
+		
+    if(message.member.roles.some(r=>["Owner"].includes(r.name)) ){
+      let author = message.author.username;
+      for(i=0;i<=args.length;i++){
+      suggestion = args.join(' ')
+      }
+
+        message.delete()
+        message.channel.send(`Your suggestion has been recorded! Vote in #suggestions!`)
+
+        let Sembed = new Discord.RichEmbed()
+        .setColor("#13edc5")
+        .setAuthor(`${message.guild.name} Suggestions`, message.guild.iconURL)
+        .setThumbnail(bot.user.displayAvatarURL)
+        .addField("Suggestion:", suggestion)
+        .addField("Suggested By:", author)
+
+        bot.guilds.get('607885235719372801').channels.get('608645820287418394').send({embed: Sembed}).then(embedMessage => {
+        embedMessage.react("👍");
+        embedMessage.react("👎");
+        });
+
+    } else if(message.member.roles.some(r=>["SuggestionBan"].includes(r.name)) ){
+
+      return message.channel.send("You have been banned from using this command!");
+
+    }
+	},
+};
