a849c1b8d1ffded1c5fb73da92568c162504a0f1
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-27 21:57:39 +0300

    Update anti_spam.js

--- anti_spam.js
+++ anti_spam.js
@@ -151,28 +151,31 @@ module.exports = async (client, options) => {
         }
       }
     } else {
-      for (var x = 0; x < profanities.length; x++) {
-        if (message.content.includes(profanities[x])){
-            await message.reply("Don't say that here! :angry:");     
-          
-            let ChatFilterEmbed = new Discord.RichEmbed()
-            .setColor("#bf1711")
-            .setTitle(`${message.author.username} Has Been Warned For Language!`);
-            message.channel.send({embed: ChatFilterEmbed});  
-            //message.delete();
-          
-            let ChatFilterModEmbed = new Discord.RichEmbed()
-            .setColor("#bf1711")
-            .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
-            .setThumbnail(bot.user.displayAvatarURL)
-            .addField("Moderation:", "Warn")
-            .addField("Warned Person:", message.author.username)
-            .addField("Reason:", "Language")
-            .addField("Moderator:", bot.user.username)
-            .addField("Date:", message.createdAt.toLocaleString())
 
-            bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: ChatFilterModEmbed});
-            return;     
+      for (x = 0; x < profanities.length; x++) {
+            if (message.content.includes(profanities[x])){
+                await message.reply("Don't say that here! :angry:")     
+              
+                let ChatFilterEmbed = new Discord.RichEmbed()
+                .setColor("#bf1711")
+                .setTitle(`${message.author.username} Has Been Warned For Language!`);
+                message.channel.send({embed: ChatFilterEmbed});  
+                  //message.delete();
+              
+                let ChatFilterModEmbed = new Discord.RichEmbed()
+                .setColor("#bf1711")
+                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+                .setThumbnail(bot.user.displayAvatarURL)
+                .addField("Moderation:", "Warn")
+                .addField("Warned Person:", message.author.username)
+                .addField("Reason:", "Language")
+                .addField("Moderator:", bot.user.username)
+                .addField("Date:", message.createdAt.toLocaleString())
+    
+                bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: ChatFilterModEmbed});
+                return;     
+      }
+    }
     }
   });
 }
