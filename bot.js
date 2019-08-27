055ff8dfccca6884a31cb40aa3de9005e75d33f5
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-27 21:47:59 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -55,6 +55,4 @@ let cmd = args.shift().toLowerCase();
 let commandfile = bot.commands.get(cmd.slice(0)) || bot.commands.get(bot.aliases.get(cmd.slice(0)))
 if (commandfile) commandfile.run(bot,message,args)
 
-)
-
 bot.login(process.env.BOT_TOKEN);
