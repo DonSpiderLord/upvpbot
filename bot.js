cfa00906045cac68f388dd136888aa2544285543
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-27 22:26:19 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -45,6 +45,31 @@ fs.readdir("./commands", (err, files) => {
 });
 
 bot.on("message", async message =>{
+  
+        for (x = 0; x < profanities.length; x++) {
+            if (message.content.includes(profanities[x])){
+                await message.reply("Don't say that here! :angry:")     
+              
+                let ChatFilterEmbed = new Discord.RichEmbed()
+                .setColor("#bf1711")
+                .setTitle(`${message.author.username} Has Been Warned For Language!`);
+                message.channel.send({embed: ChatFilterEmbed});  
+                  //message.delete();
+              
+                let ChatFilterModEmbed = new Discord.RichEmbed()
+                .setColor("#bf1711")
+                .setAuthor(`${message.guild.name} Modlogs`, message.guild.iconURL)
+                .setThumbnail(bot.user.displayAvatarURL)
+                .addField("Moderation:", "Warn")
+                .addField("Warned Person:", message.author.username)
+                .addField("Reason:", "Language")
+                .addField("Moderator:", bot.user.username)
+                .addField("Date:", message.createdAt.toLocaleString())
+    
+                bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: ChatFilterModEmbed});
+                return;     
+      }
+    }
   bot.emit('checkMessage', message);
   if (message.author.bot || message.channel.type === "dm") return;
   let args = message.content.slice(prefix.length).split(' ');
