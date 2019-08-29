5f1d9abb07031d1f9b727a940dcc3b35a55728ac
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-29 20:20:49 +0300

    Update ban.js

--- commands/ban.js
+++ commands/ban.js
@@ -6,7 +6,7 @@ module.exports.run = async (bot, message, args) => {
       if(!banee) return message.channel.send("Please supply a user to ban!");
 
       message.delete()
-      mutee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:\n===============`);
+      banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:\n===============`);
       message.channel.send(`${banee.user.username} Has Been Succesfully Banned.`);
 
       let Bembed = new Discord.RichEmbed()
