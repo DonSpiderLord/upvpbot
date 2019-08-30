634c1e1d95dbf34547bf36a671a8c24056548692
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-08-30 22:24:44 +0300

    Update tempban.js

--- commands/tempban.js
+++ commands/tempban.js
@@ -7,12 +7,12 @@ module.exports.run = async (bot, message, args) => {
       let baneeID = banee.user.id;
       
       let time = args[1];
-      if(time > 24) return message.channel.send("Max ban time is 25 seconds!");
+      if(time > 24) return message.channel.send("Max ban time is 24 Hours!");
       if(!time) return message.channel.send("Please supply how long the person will be banned!");
       
       message.delete()
       banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:\n===============`);
-      message.channel.send(`${banee.user.username} Has Been Succesfully Banned For ${time} Seconds.`);
+      message.channel.send(`${banee.user.username} Has Been Banned For ${time} Hours.`);
 
       let Bembed = new Discord.RichEmbed()
       .setColor("#0890d4")
@@ -25,8 +25,8 @@ module.exports.run = async (bot, message, args) => {
 
       bot.guilds.get('607885235719372801').channels.get('608577419527454730').send({embed: Bembed});
       banee.ban();
-      setTimeout(() => {message.guild.unban(banee)}, time * 1000);
-      setTimeout(() => {message.channel.send(`${banee.user.username} Has Been Unbanned.`)}, time * 1000);
+      setTimeout(() => {message.guild.unban(banee)}, time * 3600000);
+      setTimeout(() => {message.channel.send(`${banee.user.username} Has Been Unbanned.`)}, time * 3600000);
 
   } else {
 
