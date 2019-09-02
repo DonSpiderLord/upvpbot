6cc8666d59dc674ff8ecb87d9836e47f9b7083c7
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-02 20:14:27 +0300

    Update unban.js

--- commands/unban.js
+++ commands/unban.js
@@ -7,20 +7,21 @@ module.exports.run = async (bot, message, args) => {
         
         let user = message.mentions.members.first() || message.guild.members.get(args[0]) || args[0];
         if(!user) return message.channel.send("Please supply a user to unban.");
-        message.channel.send(user.id);
-        /*message.guild.fetchBans()
+        
+        message.guild.fetchBans()
         .then(bans => {
             if (bans.some(u => User.includes(u.username))) {
             let user = bans.find(user => user.username === User);
-                  message.guild.unban(user.id);
-                  message.channel.send(`User ${User} has been unbanned.`)
+                  //message.guild.unban(user.id);
+                  message.channel.send(user.id);
+                  //message.channel.send(`User ${User} has been unbanned.`)
          } else if (bans.some(u => User.includes(u.id))) {
-                  message.guild.unban(User);
-                  message.channel.send(`User ${User} has been unbanned.`)
+                  //message.guild.unban(User);
+                  //message.channel.send(`User ${User} has been unbanned.`)
          } else {
             return message.reply(`This person is not banned.`);
          }
-    });*/
+    });
    } else {
         return message.channel.send("You don't have permission to use this command!");
    }      
