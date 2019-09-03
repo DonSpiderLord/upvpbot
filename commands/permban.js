04315a9d3e867a77085aa43979bc17c541e20222
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-03 19:35:33 +0300

    Update permban.js

--- commands/permban.js
+++ commands/permban.js
@@ -6,7 +6,6 @@ module.exports.run = async (bot, message, args) => {
       if(!banee) return message.channel.send("Please supply a user to ban!");
 
       message.delete()
-      banee.send(`Hello, you have been banned from ${message.guild.name}.\nThis is automated message. Don't respond to it.\nHave A Nice Day :wave:\n===============`);
       message.channel.send(`${banee.user.username} Has Been Succesfully Banned.`);
 
       let Bembed = new Discord.RichEmbed()
