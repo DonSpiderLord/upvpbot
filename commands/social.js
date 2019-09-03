c2126e1f3dc26e1796cdd64b7150faec7d60b906
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-03 19:47:51 +0300

    Create social.js

---
+++ commands/social.js
@@ -0,0 +1,22 @@
+const botconfig = require("../botconfig.json");
+const Discord = require("discord.js");
+const prefix = botconfig.prefix
+
+module.exports.run = async (bot, message, args) => {
+        let sEmbed = new Discord.RichEmbed()
+        .setColor("#e6aa07")
+        .setTitle("Help Menu")
+        .setThumbnail(bot.user.displayAvatarURL)
+        //.setAuthor(bot.user.username)
+        .setDescription(`These Are Links To Our Social Media Profiles/Websites:\n\n**Discord** - _discord.gg/8hCeYgc_\n\n**Twitter** - _witter.com/UPVP_Network_\n\n**Instagram** - _https://www.instagram.com/upvp_network_\n\n**U-PvP Website** - _u-pvp.com_\n\n**Voting Sites** - _u-pvp.com/vote1_ And _u-pvp.com/vote2_`);
+        message.channel.send({embed: sEmbed});
+    }
+
+module.exports.config = {
+  name: "social",
+  aliases: ["media"],
+  usage: "u.social",
+  description: "Shows all links to social media's",
+  //noalias: "No Aliases",
+  accessableby: "Peasant"
+}
