6006e26f0751b25b73e9516dd4848221e43d77eb
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-03 18:49:04 +0300

    Create say.js

---
+++ commands/say.js
@@ -0,0 +1,22 @@
+const Discord = require("discord.js");
+
+module.exports.run = async (bot, message, args) => {
+    if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+"].includes(r.name)) ) {
+    for (i = 0; i < args.length; i++){
+        let text = args.join(' ');  
+    }
+    
+    message.channel.send(text);
+  } else {
+    return message.channel.send("You don't have permission to use this command!");
+  }
+}
+
+module.exports.config = {
+  name: "say",
+  aliases: ["sendmsg"],
+  usage: "u.say <message>",
+  description: "Kicks Specified User.",
+  //noalias: "No Aliases",
+  accessableby: "Mod+ and Above"
+}
