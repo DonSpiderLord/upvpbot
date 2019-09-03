7a24c3ba49b046597cf9e2f80947356529f51976
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-03 20:04:21 +0300

    Update banlist.js

--- commands/banlist.js
+++ commands/banlist.js
@@ -2,12 +2,15 @@ let Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
   if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin"].includes(r.name)) ) {
-    message.channel.send("Banned Users List\n===============");
+    let BanListEmbed = new Discord.RichEmbed()
+    .setTitle("Banned Users List")
+    .addField("=================")
     message.guild.fetchBans().then(bans => {
           bans.forEach(user => {
-            message.channel.send(user.tag);
+            .addField(user.tag)
           })
       })
+    message.channel.send({embed: BanListEmbed});
   } else {
   message.channel.send("You don't have permission to use this command!");
   }
