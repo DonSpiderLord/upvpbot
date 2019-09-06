53af8a4fbd105db21e4114d3d96da1506fadaac5
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-06 21:23:43 +0300

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -2,10 +2,12 @@ const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
 
-  message.channel.send("Inicializing setup...");
+  message.channel.send("Inicializing setup...").then(m => {
   setTimeout(() => {m.edit("Setup In Progress\n===============\n:black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nExtracting Commands...\n===============")}, 5 * 1000);
   setTimeout(() => {m.edit("Setup In Progress\n===============\n:white_check_mark: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nSetting Up Modules...\n===============")}, 10 * 1000);
-}
+    
+  });
+  }
 
 module.exports.config = {
   name: "setup",
