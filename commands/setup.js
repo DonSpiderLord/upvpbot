5f1032089efb684564442f5dad11b51a0bb1e89a
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-06 21:21:11 +0300

    Update setup.js

--- commands/setup.js
+++ commands/setup.js
@@ -3,8 +3,8 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
   message.channel.send("Inicializing setup...");
-  setTimeout(() => {message.edit("Setup In Progress\n===============\n:black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nExtracting Commands...\n===============")}, 5 * 1000);
-  setTimeout(() => {message.edit("Setup In Progress\n===============\n:white_check_mark: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nSetting Up Modules...\n===============")}, 10 * 1000);
+  setTimeout(() => {m.edit("Setup In Progress\n===============\n:black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nExtracting Commands...\n===============")}, 5 * 1000);
+  setTimeout(() => {m.edit("Setup In Progress\n===============\n:white_check_mark: :black_square_button: :black_square_button: :black_square_button: :black_square_button: :black_square_button:\nSetting Up Modules...\n===============")}, 10 * 1000);
 }
 
 module.exports.config = {
