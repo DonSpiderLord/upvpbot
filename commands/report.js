93a9d2a5cc095edd5b9169a3204b14858267ad83
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-09 23:21:39 +0300

    Update report.js

--- commands/report.js
+++ commands/report.js
@@ -4,17 +4,19 @@ const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
 
 module.exports.run = async (bot, message, args) => {
-    if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+", "Mod", "Trial-Mod", "Helper", "Peasant", "PvP God"].includes(r.name)) ){
+    if(message.member.roles.some(r=>["Report Ban"].includes(r.name)) ){
+      return message.channel.send("You have been banned from using this command!");
+    } else if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+", "Mod", "Trial-Mod", "Helper", "Peasant", "PvP God"].includes(r.name)) ){
       let author = message.author.username;
       let rUser = args[0];
-       if(!rUser) return message.channel.send("Please supply a user to report!");
+      if(!rUser) return message.channel.send("Please supply a user to report!");
       let reason = args[1];
-       if(!reason) reason = "Not Defined";
+      if(!reason) reason = "Not Defined";
       let server = args[2];
-       if(!server) server = "Not Defined";
+      if(!server) server = "Not Defined";
 
-        message.delete()
-        message.channel.send(`${rUser} was successfully reported.`)
+      message.delete()
+      message.channel.send(`${rUser} was successfully reported.`)
 
         let Rembed = new Discord.RichEmbed()
         .setColor("#ffbb00")
@@ -26,10 +28,7 @@ module.exports.run = async (bot, message, args) => {
         .addField("Reported By:", author)
 
         bot.guilds.get('569546798725726236').channels.get('619571632553590825').send({embed: Rembed});
-
-    } else if(message.member.roles.some(r=>["ReportBan"].includes(r.name)) ){
-
-      return message.channel.send("You have been banned from using this command!");
+      
 
     }
 }
