87e4e5e479bdb8a3b227a122eefd356883c4a7d1
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-09 16:28:46 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -8,13 +8,13 @@ bot.on("ready", async () =>{
   antispam(bot, {
         warnBuffer: 3, // Maximum ammount of messages allowed to send in the interval time before getting warned.
         maxBuffer: 5, // Maximum amount of messages allowed to send in the interval time before getting banned.
-        interval: 3000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting banned.
+        interval: 5000, // Amount of time in ms users can send the maxim amount of messages(maxBuffer) before getting muted.
         warningMessage: "stop spamming! :oncoming_police_car:", // Message users receive when warned. (message starts with '@User, ' so you only need to input continue of it.)
         banMessage: "has been banned for spamming!", // Message sent in chat when user is banned. (message starts with '@User, ' so you only need to input continue of it.)
         maxDuplicatesWarning: 3,// Maximum amount of duplicate messages a user can send in a timespan before getting warned.
         maxDuplicatesBan: 5, // Maximum amount of duplicate messages a user can send in a timespan before getting banned.
         deleteMessagesAfterBanForPastDays: 7, // Deletes the message history of the banned user in x days.
-        exemptRoles: ["Owner", "Co-Owner", "Manager", "Build Manager", "Developer", "Admin", "Moderator", "Trial-Mod", "Helper", "Builder", "Trial Builder", "Staff"], // Name of roles (case sensitive) that are exempt from spam filter.
+        exemptRoles: ["Owner", "Co-Owner", "Manager", "Build Manager", "Developer", "Admin","Mod+", "Mod", "Trial-Mod", "Helper", "Builder", "Trial Builder", "Staff"], // Name of roles (case sensitive) that are exempt from spam filter.
         exemptUsers: [] // The Discord tags of the users (e.g: MrAugu#9016) (case sensitive) that are exempt from spam filter.
       });
   console.log(`${bot.user.username} is online!`);
