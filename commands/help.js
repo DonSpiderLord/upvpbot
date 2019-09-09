4a455998a7add7ce7d0993c870e8cc8290326285
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-09 17:00:52 +0300

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -4,7 +4,7 @@ const prefix = botconfig.prefix
 
 module.exports.run = async (bot, message, args) => {
         let sEmbed = new Discord.RichEmbed()
-        .setColor("#e6aa07")
+        .setColor("#ffffff")
         .setTitle("Help Menu")
         .setThumbnail(bot.user.displayAvatarURL)
         //.setAuthor(bot.user.username)
