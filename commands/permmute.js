10245a1da53d2bdb1e21086ba46c367b8af4dd9b
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-09 20:21:32 +0300

    Update permmute.js

--- commands/permmute.js
+++ commands/permmute.js
@@ -1,7 +1,7 @@
 const Discord = require("discord.js");
 
 module.exports.run = async (bot, message, args) => {
-  if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+"].includes(r.name)) ) {
+  if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin"].includes(r.name)) ) {
 
     let mutee = message.mentions.members.first() || message.guild.members.get(args[0]);
     if(!mutee) return message.channel.send("Please supply a user to mute!");
