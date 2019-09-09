888100096cfaea60cbd789c7c37eff28203394b9
DonSpiderLord <53938497+DonSpiderLord@users.noreply.github.com>
2019-09-09 23:16:52 +0300

    Update suggest.js

--- commands/suggest.js
+++ commands/suggest.js
@@ -4,7 +4,10 @@ const botconfig = require("../botconfig.json");
 const prefix = botconfig.prefix;
 
 module.exports.run = async (bot, message, args) => {
-    if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+", "Mod", "Trial-Mod", "Helper", "Peasant", "PvP God"].includes(r.name)) ){
+    if(message.member.roles.some(r=>["SuggestionBan"].includes(r.name)) ){
+      return message.channel.send("You have been banned from using this command!");
+    } else if(message.member.roles.some(r=>["Owner", "Co-Owner", "Manager", "Admin", "Mod+", "Mod", "Trial-Mod", "Helper", "Peasant", "PvP God"].includes(r.name)) && message.member.roles.find(r=>["Peasant"].includes(r.name))) {
+      
       let author = message.author.username;
       for(i=0;i<=args.length;i++){
       suggestion = args.join(' ')
@@ -25,10 +28,6 @@ module.exports.run = async (bot, message, args) => {
 		  .then(() => embedMessage.react('❌'))
         });
 
-    } else if(message.member.roles.find(r=>["SuggestionBan"].includes(r.name)) ){
-      return message.channel.send("You have been banned from using this command!");
-    } else if(message.member.roles.find(r=>["SuggestionBan"].includes(r.name)) && message.member.roles.find(r=>["Peasant"].includes(r.name))) {
-      return message.channel.send("You have been banned from using this command!");
     }
 }
 
